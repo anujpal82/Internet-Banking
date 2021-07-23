39de5ec675813f44a84061316967f9c7495838f7
anujpal82 <anujpal631998@gmail.com>
2021-07-23 17:30:53 +0530

    dc

--- AnujPal/FinalProject/Backend/Node/Web API/Project/server.js
+++ AnujPal/FinalProject/Backend/Node/Web API/Project/server.js
@@ -10,15 +10,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/home", router);
 
-app.listen(5000, (err) => {
+app.listen(process.env.PORT || 5000, (err) => {
   if (err) {
     console.error("Error occured during starting server");
   }
-  console.log("Server started on port 5000");
-});
-
-app.get("", (req, res) => {
-  res.json("hello");
+  console.log("Server started ");
 });
 
 mongoose.connect("mongodb://localhost/Project", {
